7cb6e93004bab6c20f414cb15101509f98035f12
cho881020 <kj_cho@idealidea.co.kr>
2020-04-22 21:29:21 +0900

    lateinit으로 어댑터 초기화 / 리스트뷰 반영

--- app/src/main/java/kr/tjeit/pizzastoreapp_20200422/MainActivity.kt
+++ app/src/main/java/kr/tjeit/pizzastoreapp_20200422/MainActivity.kt
@@ -3,9 +3,14 @@ package kr.tjeit.pizzastoreapp_20200422
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.tjeit.pizzastoreapp_20200422.adapters.StoreAdapter
 import kr.tjeit.pizzastoreapp_20200422.datas.Store
 
 class MainActivity : BaseActivity() {
+
+    private lateinit var mPizzaAdapter:StoreAdapter
+    val pizzaStores = ArrayList<Store>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +24,17 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        addPizzaStores()
+        mPizzaAdapter = StoreAdapter(mContext, R.layout.store_list_item, pizzaStores)
 
+        pizzaStoreListView.adapter = mPizzaAdapter
+    }
+
+    fun addPizzaStores() {
+        pizzaStores.add(Store("피자헛", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg", "1111-2222"))
+        pizzaStores.add(Store("파파존스", "http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800", "3333-4444"))
+        pizzaStores.add(Store("미스터피자", "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200", "8520-1478"))
+        pizzaStores.add(Store("도미노피자", "https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png", "9999-8888"))
     }
 
 }
