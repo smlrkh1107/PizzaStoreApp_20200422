90339477545b35bf1fcea5438b96f010a9af7970
cho881020 <kj_cho@idealidea.co.kr>
2020-04-22 21:05:33 +0900

    가게목록 어댑터 뼈대 작업

---
+++ app/src/main/java/kr/tjeit/pizzastoreapp_20200422/adapters/StoreAdapter.kt
@@ -0,0 +1,34 @@
+package kr.tjeit.pizzastoreapp_20200422.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kr.tjeit.pizzastoreapp_20200422.R
+import kr.tjeit.pizzastoreapp_20200422.datas.Store
+
+class StoreAdapter(context: Context, resId:Int, list:ArrayList<Store>) : ArrayAdapter<Store>(context, resId, list) {
+
+    val mContext = context
+    val mList = list
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        tempRow?.let {
+
+        }.let {
+            tempRow = inf.inflate(R.layout.store_list_item, null)
+        }
+
+        val row = tempRow!!
+
+
+        return row
+
+    }
+
+
+}
